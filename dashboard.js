a541c4a582d341b51f760db694f28915dfa4ffc6
sumet67 <sumet67@gmail.com>
2026-03-28 14:31:37 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,10 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzWrYAmYGat5KF3WCsVEAlzz5SEzDGa8XtfZ45KhL7FrdXT3TivqNeCLQHwctKRFzi5Q/exec";
+const SCRIPT_URL = "ใส่ URL Apps Script";
 
 async function loadData() {
     const res = await fetch(SCRIPT_URL);
     const data = await res.json();
 
     const table = document.getElementById("tableBody");
-    table.innerHTML = "";
 
     data.reverse().forEach(row => {
         table.innerHTML += `
