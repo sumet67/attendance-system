6fec1eeb8186bab820a2500ff6306a31ea772d62
sumet67 <sumet67@gmail.com>
2026-03-28 12:48:22 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -2,7 +2,7 @@
  * dashboard.js - ควบคุมการดึงข้อมูลประวัติการสแกน
  */
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzBEFGmB0zUNdS-aiVry6xVHBT8Woh8RmTuWhZjz_fnwbo4SU0sC_s9P2SdjEJb9RmohA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwb9uoyooz4k310OOkW2C5UTvK0xoSoqGgEnxxajv6sVCniSoM7EiAYsFPmTDY9qWPRIA/exec";
 
 async function loadData() {
     if (SCRIPT_URL.includes("YOUR_APPS_SCRIPT")) return;
