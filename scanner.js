18bdc735872df0bc342757fcac749b46e73ae56d
sumet67 <sumet67@gmail.com>
2026-03-28 12:48:35 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -3,7 +3,7 @@
  */
 
 // *** สำคัญที่สุด: ตรวจสอบ URL นี้ให้ถูกต้อง (ต้องลงท้ายด้วย /exec) ***
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzBEFGmB0zUNdS-aiVry6xVHBT8Woh8RmTuWhZjz_fnwbo4SU0sC_s9P2SdjEJb9RmohA/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwb9uoyooz4k310OOkW2C5UTvK0xoSoqGgEnxxajv6sVCniSoM7EiAYsFPmTDY9qWPRIA/exec"; 
 
 const video = document.getElementById('video');
 const faceFrame = document.getElementById('faceFrame');
